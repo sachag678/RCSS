4ff173e592519ff83f7a4303644a30799bde6772
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-05-10 15:07:10 -0400

    Update rungame.bat

--- rungame.bat
+++ rungame.bat
@@ -1,3 +1,3 @@
-START C:\Users\sachagunaratne\Desktop\RCSS\rcssserver-14.0.3-win\rcssserver.exe
+START rcssserver-14.0.3-win\rcssserver.exe
 timeout /t 3
-START C:\Users\sachagunaratne\Desktop\RCSS\rcssmonitor-14.0.1-win\rcssmonitor.exe
+START rcssmonitor-14.0.1-win\rcssmonitor.exe
